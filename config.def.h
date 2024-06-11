b6b51444162fa73a1e44e1c1e896670ddcccb173
Patricio <parada.rios.patricio@gmail.com>
2024-06-11 18:33:05 -0300

    patched bar-height

--- config.def.h
+++ config.def.h
@@ -3,8 +3,11 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static const int user_bh = 40;               /* add an defined amount of pixels to the bar height */
+
 static const char *fonts[] = {
-	"monospace:size=10"
+	/*"monospace:size=10"*/
+    "FiraMono Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
