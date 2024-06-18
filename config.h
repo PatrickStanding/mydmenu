fe67736d61fbfc25e273bbb2025fd38d04ff682b
Patricio <parada.rios.patricio@gmail.com>
2024-06-18 16:53:28 -0300

    working

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = -4;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 5;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
 	/*"monospace:size=10"*/
@@ -27,3 +27,6 @@ static unsigned int columns    = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
